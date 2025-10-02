da46e5a4f3b971838e11b681eadf58dd41cfad6c
Sriraj Guptha <rajguptha1213@gmail.com>
2025-10-01 21:16:32 -0400

    url

--- src/main/java/com/example/basicgamearena/BasicGameArenaApplication.java
+++ src/main/java/com/example/basicgamearena/BasicGameArenaApplication.java
@@ -2,10 +2,29 @@ package com.example.basicgamearena;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BasicGameArenaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BasicGameArenaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }
